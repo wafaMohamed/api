30e782ade3c0b4714bd9a01b71c709befa4bfa1b
wafa <wafamohameddd@gmail.com>
2024-05-23 17:28:37 +0300

    sign in logic

--- lib/cubit/user_cubit.dart
+++ lib/cubit/user_cubit.dart
@@ -1,28 +1,55 @@
 import 'package:api_2/cubit/user_state.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserCubit extends Cubit<UserState> {
   UserCubit() : super(UserInitial());
+
   //Sign in Form key
   GlobalKey<FormState> signInFormKey = GlobalKey();
+
   //Sign in email
   TextEditingController signInEmail = TextEditingController();
+
   //Sign in password
   TextEditingController signInPassword = TextEditingController();
+
   //Sign Up Form key
   GlobalKey<FormState> signUpFormKey = GlobalKey();
+
   //Profile Pic
   XFile? profilePic;
+
   //Sign up name
   TextEditingController signUpName = TextEditingController();
+
   //Sign up phone number
   TextEditingController signUpPhoneNumber = TextEditingController();
+
   //Sign up email
   TextEditingController signUpEmail = TextEditingController();
+
   //Sign up password
   TextEditingController signUpPassword = TextEditingController();
+
   //Sign up confirm password
   TextEditingController confirmPassword = TextEditingController();
+
+  Future<void> signIn() async {
+    try {
+      emit(SignInLoading());
+      final response = await Dio().post(
+        'https://food-api-omega.vercel.app/api/v1/user/signin',
+        data: {"email": signInEmail.text, "password": signInPassword.text},
+      );
+      emit(SignInSuccess());
+
+      print(response);
+    } catch (error) {
+      emit(SignInFailure(errorMessage: error.toString()));
+      print("/////////Error: ${error.toString()}");
+    }
+  }
 }
--- lib/pages/sign_in_screen.dart
+++ lib/pages/sign_in_screen.dart
@@ -1,8 +1,8 @@
-import 'package:api_2/pages/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../cubit/user_cubit.dart';
+import '../cubit/user_state.dart';
 import '../widgets/custom_form_button.dart';
 import '../widgets/custom_input_field.dart';
 import '../widgets/dont_have_an_account.dart';
@@ -17,68 +17,90 @@ class SignInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color(0xffEEF1F3),
-        body: Column(
-          children: [
-            const PageHeader(),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(20),
+      child: BlocConsumer<UserCubit, UserState>(
+          listener: (BuildContext context, UserState state) {
+        if (state is SignInSuccess) {
+          showSnackBar(context, 'Sign In Success');
+        } else if (state is SignInFailure) {
+          showSnackBar(context, state.errorMessage);
+        }
+      }, builder: (BuildContext context, UserState state) {
+        return Scaffold(
+          backgroundColor: const Color(0xffEEF1F3),
+          body: Column(
+            children: [
+              const PageHeader(),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
                   ),
-                ),
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: context.read<UserCubit>().signInFormKey,
-                    child: Column(
-                      children: [
-                        const PageHeading(title: 'Sign-in'),
-                        //!Email
-                        CustomInputField(
-                          labelText: 'Email',
-                          hintText: 'Your email',
-                          controller: context.read<UserCubit>().signInEmail,
-                        ),
-                        const SizedBox(height: 16),
-                        //!Password
-                        CustomInputField(
-                          labelText: 'Password',
-                          hintText: 'Your password',
-                          obscureText: true,
-                          suffixIcon: true,
-                          controller: context.read<UserCubit>().signInPassword,
-                        ),
-                        const SizedBox(height: 16),
-                        //! Forget password?
-                        ForgetPasswordWidget(size: size),
-                        const SizedBox(height: 20),
-                        //!Sign In Button
-                        CustomFormButton(
-                          innerText: 'Sign In',
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const ProfileScreen(),
-                              ),
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 18),
-                        //! Dont Have An Account ?
-                        DontHaveAnAccountWidget(size: size),
-                        const SizedBox(height: 20),
-                      ],
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: context.read<UserCubit>().signInFormKey,
+                      child: Column(
+                        children: [
+                          const PageHeading(title: 'Sign-in'),
+                          //!Email
+                          CustomInputField(
+                            labelText: 'Email',
+                            hintText: 'Your email',
+                            controller: context.read<UserCubit>().signInEmail,
+                          ),
+                          const SizedBox(height: 16),
+                          //!Password
+                          CustomInputField(
+                            labelText: 'Password',
+                            hintText: 'Your password',
+                            obscureText: true,
+                            suffixIcon: true,
+                            controller:
+                                context.read<UserCubit>().signInPassword,
+                          ),
+                          const SizedBox(height: 16),
+                          //! Forget password?
+                          ForgetPasswordWidget(size: size),
+                          const SizedBox(height: 20),
+                          //!Sign In Button
+                          state is SignInLoading
+                              ? const Center(child: CircularProgressIndicator())
+                              : CustomFormButton(
+                                  innerText: 'Sign In',
+                                  onPressed: () {
+                                    context.read<UserCubit>().signIn();
+                                    // Navigator.push(
+                                    //   context,
+                                    //   MaterialPageRoute(
+                                    //     builder: (context) =>
+                                    //         const ProfileScreen(),
+                                    //   ),
+                                    // );
+                                  },
+                                ),
+                          const SizedBox(height: 18),
+                          //! Dont Have An Account ?
+                          DontHaveAnAccountWidget(size: size),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+
+  void showSnackBar(BuildContext context, String text) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(text),
       ),
     );
   }
